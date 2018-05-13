e4147e9035f2340a10fd2a2641fc22d1c03d17c5
Shakhzod <vip.shaxi@gmail.com>
2018-05-13 20:06:00 +0500

    commit

---
+++ src/Server.h
@@ -0,0 +1,7 @@
+// Arduino RBD Serial Manager Library v0.1 - A simple interface for serial communication.
+// https://github.com/shakh13/SerialManager
+// Shaxzod Saidmurodov
+// MIT License
+
+#ifndef SERIAL_MANAGER
+#define SERIAL_MANAGER
