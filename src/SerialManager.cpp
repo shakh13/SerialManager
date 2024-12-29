7f1f93fd90dcee26d182fc03e70948e8fae52db2
Shaxzod <vip.shaxi@gmail.com>
2024-12-29 13:34:45 +0500

    update serial

--- src/SerialManager.cpp
+++ src/SerialManager.cpp
@@ -6,68 +6,85 @@
 #include <Arduino.h>
 #include <SerialManager.h> // https://github.com/shakh13/SerialManager
 
-  SerialManager::SerialManager() {}
-  void SerialManager::start() {
-		Serial.begin(115200);
-  }
+SerialManager::SerialManager() {}
+void SerialManager::start()
+{
+  Serial.begin(115200);
+}
 
-  bool SerialManager::onReceive() {
-		if(Serial.available()) {
-		  _char = char(Serial.read());
+bool SerialManager::onReceive()
+{
+  if (Serial.available())
+  {
+    _char = char(Serial.read());
 
-		  if(_char == _flag) {
-			_value  = _buffer;
-			_buffer = "";
-			return true;
-		  }
-		  else {
-			_buffer += _char;
-			return false;
-		  }
-		}
-		else {
-		  return false;
-		}
-  }
-
-  String SerialManager::getValue() {
-    return _value;
-  }
-
-  void SerialManager::setFlag(char value) {
-    _flag = value;
-  }
-
-  void SerialManager::setDelimiter(char value) {
-    _delimiter = value;
-  }
-
-  String SerialManager::getCmd() {
-    _position = getValue().indexOf(_delimiter);
-
-    if(_position > -1) {
-      return _value.substring(0, _position);
+    if (_char == _flag)
+    {
+      _value = _buffer;
+      _buffer = "";
+      return true;
     }
-    else {
-      return getValue();
+    else
+    {
+      _buffer += _char;
+      return false;
     }
   }
-
-  String SerialManager::getParam() {
-    _position = getValue().indexOf(_delimiter);
-
-    if(_position > -1) {
-      return _value.substring(_position + 1, _value.length());
-    }
-    else {
-      return "";
-    }
+  else
+  {
+    return false;
   }
+}
 
-  bool SerialManager::isCmd(String value) {
-    return getCmd() == value;
-  }
+String SerialManager::getValue()
+{
+  return _value;
+}
 
-  bool SerialManager::isParam(String value) {
-    return getParam() == value;
+void SerialManager::setFlag(char value)
+{
+  _flag = value;
+}
+
+void SerialManager::setDelimiter(char value)
+{
+  _delimiter = value;
+}
+
+String SerialManager::getCmd()
+{
+  _position = getValue().indexOf(_delimiter);
+
+  if (_position > -1)
+  {
+    return _value.substring(0, _position);
   }
+  else
+  {
+    return getValue();
+  }
+}
+
+String SerialManager::getParam()
+{
+  _position = getValue().indexOf(_delimiter);
+
+  if (_position > -1)
+  {
+    return _value.substring(_position + 1, _value.length());
+  }
+  else
+  {
+    return "";
+  }
+}
+
+bool SerialManager::isCmd(String value)
+{
+  return getCmd() == value;
+}
+
+bool SerialManager::isParam(String value)
+{
+  return getParam() == value;
+}
